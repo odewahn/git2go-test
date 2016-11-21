49697cf337afc8aaf4243e2324c4c7ffa50661c1
Andrew Odewahn <andrew@odewahn.com>
2016-11-21 17:22:23 -0500

    adding clone for https with auth

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
-import "github.com/libgit2/git2go"
+import (
+	"fmt"
+	"log"
+	"os"
 
-// https://golog.co/blog/article/Git2Go
+	"github.com/libgit2/git2go"
+)
+
+// Useful articles:
+//   git2go godocs: https://godoc.org/github.com/libgit2/git2go
+//   private git access for libgit2: https://golog.co/blog/article/Git2Go
+//   default ssh keyfile names: https://help.github.com/articles/checking-for-existing-ssh-keys
 
 func credentialsCallback(url string, username string, allowedTypes git.CredType) (git.ErrorCode, *git.Cred) {
-	ret, cred := git.NewCredSshKey("git", "/Users/odewahn/.ssh/id_rsa.pub", "/Users/odewahn/.ssh/id_rsa", "")
+	//ret, cred := git.NewCredSshKey("git", "/Users/odewahn/.ssh/id_rsa.pub", "/Users/odewahn/.ssh/id_rsa", "")
+	fmt.Print("Enter username: ")
+	var user string
+	fmt.Scanln(&user)
+
+	fmt.Print("Enter password: ")
+	var password string
+	fmt.Scanln(&password)
+
+	ret, cred := git.NewCredUserpassPlaintext(user, password)
 	return git.ErrorCode(ret), &cred
 }
 
@@ -15,6 +33,9 @@ func certificateCheckCallback(cert *git.Certificate, valid bool, hostname string
 
 func main() {
 
+	//url := "git@git.atlas.oreilly.com:landers/lora-using-arduino.git"
+	//url := "https://git.atlas.oreilly.com/landers/lora-using-arduino.git"
+
 	cloneOptions := &git.CloneOptions{}
 	cloneOptions.FetchOptions = &git.FetchOptions{
 		RemoteCallbacks: git.RemoteCallbacks{
@@ -23,12 +44,8 @@ func main() {
 		},
 	}
 
-	_, err := git.Clone(
-		"git@git.atlas.oreilly.com:landers/lora-using-arduino.git",
-		"web",
-		cloneOptions,
-	)
+	_, err := git.Clone(os.Args[1], "web", cloneOptions)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 }
